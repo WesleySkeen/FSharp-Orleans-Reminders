ff3977ed3f9aa7dc2583124ef970fa16fe438d80
Wesley Skeen <wesley.skeen@emailage.com>
2022-11-10 16:50:54 +0000

    Run orleans silo

--- FSharp.Orleans.Reminders/Program.fs
+++ FSharp.Orleans.Reminders/Program.fs
@@ -1,2 +1,29 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿
+open System
+open Orleans
+open Orleans.Configuration
+open Orleans.Hosting
+open System.Net
+open OrleansDashboard
+
+[<EntryPoint>]
+let main args =
+    let theTask = task {
+        let host = SiloHostBuilder()
+                        .Configure(fun (x : ClusterOptions) -> 
+                            x.ClusterId <- "fsharp-orleans-reminders-cluster-id"
+                            x.ServiceId <- "fsharp-orleans-reminders-service-id")
+                        .UseLocalhostClustering()
+                        .UseDashboard(fun (x : DashboardOptions) -> 
+                            x.HostSelf <- true
+                            x.Port <- 9090)
+                        .ConfigureEndpoints(IPAddress.Loopback, 2020, 4040)
+                        .Build()
+                 
+        do! host.StartAsync ()        
+        printfn "App is now running"
+        Console.ReadLine () |> ignore
+    } 
+    theTask.Wait()
+    0
+    

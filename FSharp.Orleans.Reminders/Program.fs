e5d04524e2d33fd9221049e7baba7d2ed37e4ac4
Wesley Skeen <wesley.skeen@emailage.com>
2022-11-11 12:04:11 +0000

    Added code gen as it is required

--- FSharp.Orleans.Reminders/Program.fs
+++ FSharp.Orleans.Reminders/Program.fs
@@ -1,6 +1,11 @@
 ﻿
 open System
+open FSharp.Orleans.Reminders.GrainActivatorHostedService
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
 open Orleans
+
+open Orleans.ApplicationParts
 open Orleans.Configuration
 open Orleans.Hosting
 open System.Net
@@ -9,16 +14,28 @@ open OrleansDashboard
 [<EntryPoint>]
 let main args =
     let theTask = task {
-        let host = SiloHostBuilder()
-                        .Configure(fun (x : ClusterOptions) -> 
-                            x.ClusterId <- "fsharp-orleans-reminders-cluster-id"
-                            x.ServiceId <- "fsharp-orleans-reminders-service-id")
-                        .UseLocalhostClustering()
-                        .UseDashboard(fun (x : DashboardOptions) -> 
-                            x.HostSelf <- true
-                            x.Port <- 9090)
-                        .ConfigureEndpoints(IPAddress.Loopback, 2020, 4040)
-                        .Build()
+        
+        let host = HostBuilder()
+                       .UseOrleans(fun (builder : ISiloBuilder) ->
+                            builder
+                                .Configure(fun (clusterOptions:ClusterOptions) ->
+                                    clusterOptions.ClusterId <- "fsharp-orleans-reminders-cluster-id"
+                                    clusterOptions.ServiceId <- "fsharp-orleans-reminders-service-id"
+                                )
+                                .UseDashboard(fun (x : DashboardOptions) -> 
+                                    x.HostSelf <- true
+                                    x.Port <- 9090)
+                                .UseLocalhostClustering()
+                                .UseInMemoryReminderService()
+                                .ConfigureEndpoints(IPAddress.Loopback, 2020, 4040)
+                                .ConfigureServices(fun (serviceCollection:IServiceCollection) ->
+                                    serviceCollection.AddHostedService<GrainActivatorHostedService>()                                    
+                                    |> ignore
+                                ).ConfigureApplicationParts (fun(applicationPartManager:IApplicationPartManager) ->
+                                    applicationPartManager.ConfigureDefaults |> ignore
+                                ) |> ignore
+                            |> ignore
+                        ).Build()               
                  
         do! host.StartAsync ()        
         printfn "App is now running"
